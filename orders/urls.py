a0580826b9a7ecb4fb1ce64b1c16d5dcdf9cd8fc
Artur Bauer <bauerart1@gmail.com>
2020-03-24 22:35:38 +0100

    Add footer, about us and update header

--- orders/urls.py
+++ orders/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("menu", views.menu_view, name="menu"),
+    path("about_us", views.about_us_view, name="about_us"),
     path("login", views.login_view, name="login"),
+    path("login_auth", views.login_authenticate, name="login_auth"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register_view, name="register"),
-    path("menu", views.menu_view, name="menu"),
     path("orders", views.orders_view, name="orders")
 ]
--- orders/views.py
+++ orders/views.py
@@ -7,16 +7,19 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    if not request.user.is_authenticated:
-        return render(request, "orders/login.html", {"message": None})
-    context = {
-        "user": request.user
-    }
     return render(request, "orders/index.html")
 
 
 def login_view(request):
     return render(request, "orders/login.html", {"message": None})
+
+
+def menu_view(request):
+    return render(request, 'orders/menu.html')
+
+
+def about_us_view(request):
+    return render(request, 'orders/about_us.html')
 
 
 def login_authenticate(request):
@@ -49,10 +52,6 @@ def register_view(request):
         form = UserCreationForm()
     print('hi')
     return render(request, 'orders/register.html', {'form': form})
-
-
-def menu_view(request):
-    return render(request, 'orders/menu.html')
 
 
 def orders_view(request):
