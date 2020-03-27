8c8ecdbff2c818124684c0ba8af5c1c119276c67
Artur Bauer <bauerart1@gmail.com>
2020-03-27 12:23:07 +0100

    Add quantity and auto sum

--- orders/static/orders/js/cart.js
+++ orders/static/orders/js/cart.js
@@ -1,17 +1,29 @@
 $(document).ready(() => {
     update_Prices();
-    $('.large').change(() => {
+    $('input').change(() => {
         update_Prices();
     });
 });
 
 function update_Prices() {
     $('.item-cart').each(function () {
-        console.log($(this).find(".large").prop('checked'));
         if ($(this).find(".large").prop('checked')) {
             $(this).find('.price').html($(this).find('.price-large').html());
         } else {
             $(this).find('.price').html($(this).find('.price-small').html());
         }
+        if ($(this).find('.extra-cheese').prop('checked')) {
+            $(this).find('.price').html(parseFloat($(this).find('.price').html()) + 0.5);
+        }
+        $(this).find('.price').html(parseFloat($(this).find('.price').html()) * parseFloat($(this).find('.quantity').val()));
     });
+    update_total();
+}
+
+function update_total() {
+    let total = 0;
+    $('.price').each(function () {
+        total += parseFloat($(this).html());
+    });
+    $('.total-price').html(total);
 }
