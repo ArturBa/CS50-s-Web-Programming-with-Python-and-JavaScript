594a846b3d410cbb2be7634fc122a4f249cb8174
Artur Bauer <bauerart1@gmail.com>
2020-05-28 15:43:15 +0200

    Add login and logout js handlers

--- final/forum/static/forum/js/base.js
+++ final/forum/static/forum/js/base.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
-    $('#login').submit(function (e) {
+    $('#login').submit(function (event) {
         console.log('trying to login');
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/login/',
+            data: $('#login').serialize(),
+            success: function () {
+                location.reload();
+            },
+            statusCode: {
+                401: function () {
+                    alert('Invalid credentials');
+                }
+            }
+        });
     });
+    $('#logout').click(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '/logout/',
+                data: $('#logout-token').serialize(),
+                success: function () {
+                    location.reload();
+                }
+            });
+        }
+    );
 })
 
-function logout() {
-    console.log('trying to logout');
-}
